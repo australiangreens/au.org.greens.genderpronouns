d62522c18b8ebb8f1f5ff0272c242e77446dc99e
Seamus Lee <seamuslee001@gmail.com>
2019-07-29 12:12:33 +1000

    Initial commit of Gender Pronouns extension

---
+++ CRM/Genderpronouns/Upgrader.php
@@ -0,0 +1,178 @@
+<?php
+use CRM_Genderpronouns_ExtensionUtil as E;
+
+/**
+ * Collection of upgrade steps.
+ */
+class CRM_Genderpronouns_Upgrader extends CRM_Genderpronouns_Upgrader_Base {
+
+  // By convention, functions that look like "function upgrade_NNNN()" are
+  // upgrade tasks. They are executed in order (like Drupal's hook_update_N).
+
+  /**
+   * Example: Run an external SQL script when the module is installed.
+   */
+  public function install() {
+    $customGroupParams = [
+      'title' => E::ts('Contact Gender Pronouns'),
+      'name' => 'contact_gender_pronouns',
+      'extends' => 'contacts',
+      'is_active' => 1,
+      'is_reserved' => 1,
+      'style' => 'Inline',
+      'collapse_display' => 1,
+      'is_public' => 1,
+    ];
+    $customGroup = civicrm_api3('CustomGroup', 'create', $customGroupParams);
+    $customFieldParams = [
+      'title' => E::ts('Gender Pronoun'),
+      'name' => 'gender_pronoun',
+      'data_type' => 'String',
+      'html_type' => 'Text',
+      'is_searchable' => 1,
+      'is_active' => 1,
+      'note_columns' => 60,
+      'note_rows' => 4,
+      'weight' => 1,
+      'custom_group_id' => $customGroup['id'],
+    ];
+    $customField = civicrm_api3('CustomField', 'create', $customFieldParams);
+    $optionGroup = civicrm_api3('OptionGroup', 'create', [
+      'label' => E::ts('Gender Pronouns'),
+      'name' => 'gender_pronouns',
+      'is_resevered' => 1,
+      'data_type' => 'Integer',
+      'description' => E::ts('Standard Gender Pronouns'),
+      'is_active' => 1,
+    ]);
+    $standardOptions = [
+      1 => 'He/Him',
+      2 => 'She/Her',
+      3 => 'Their/They',
+    ];
+    foreach ($standardOptions as $value => $label) {
+      civicrm_api3('OptionValue', 'create', [
+        'label' => E::Ts('%1', [1 => $label]),
+        'name' => $label,
+        'value' => $value,
+        'option_group_id' => 'gender_pronouns',
+      ]);
+    }
+  }
+
+  /**
+   * Example: Work with entities usually not available during the install step.
+   *
+   * This method can be used for any post-install tasks. For example, if a step
+   * of your installation depends on accessing an entity that is itself
+   * created during the installation (e.g., a setting or a managed entity), do
+   * so here to avoid order of operation problems.
+   *
+  public function postInstall() {
+    $customFieldId = civicrm_api3('CustomField', 'getvalue', array(
+      'return' => array("id"),
+      'name' => "customFieldCreatedViaManagedHook",
+    ));
+    civicrm_api3('Setting', 'create', array(
+      'myWeirdFieldSetting' => array('id' => $customFieldId, 'weirdness' => 1),
+    ));
+  }
+
+  /**
+   * Example: Run an external SQL script when the module is uninstalled.
+   *
+  public function uninstall() {
+   $this->executeSqlFile('sql/myuninstall.sql');
+  }
+
+  /**
+   * Example: Run a simple query when a module is enabled.
+   *
+  public function enable() {
+    CRM_Core_DAO::executeQuery('UPDATE foo SET is_active = 1 WHERE bar = "whiz"');
+  }
+
+  /**
+   * Example: Run a simple query when a module is disabled.
+   *
+  public function disable() {
+    CRM_Core_DAO::executeQuery('UPDATE foo SET is_active = 0 WHERE bar = "whiz"');
+  }
+
+  /**
+   * Example: Run a couple simple queries.
+   *
+   * @return TRUE on success
+   * @throws Exception
+   *
+  public function upgrade_4200() {
+    $this->ctx->log->info('Applying update 4200');
+    CRM_Core_DAO::executeQuery('UPDATE foo SET bar = "whiz"');
+    CRM_Core_DAO::executeQuery('DELETE FROM bang WHERE willy = wonka(2)');
+    return TRUE;
+  } // */
+
+
+  /**
+   * Example: Run an external SQL script.
+   *
+   * @return TRUE on success
+   * @throws Exception
+  public function upgrade_4201() {
+    $this->ctx->log->info('Applying update 4201');
+    // this path is relative to the extension base dir
+    $this->executeSqlFile('sql/upgrade_4201.sql');
+    return TRUE;
+  } // */
+
+
+  /**
+   * Example: Run a slow upgrade process by breaking it up into smaller chunk.
+   *
+   * @return TRUE on success
+   * @throws Exception
+  public function upgrade_4202() {
+    $this->ctx->log->info('Planning update 4202'); // PEAR Log interface
+
+    $this->addTask(E::ts('Process first step'), 'processPart1', $arg1, $arg2);
+    $this->addTask(E::ts('Process second step'), 'processPart2', $arg3, $arg4);
+    $this->addTask(E::ts('Process second step'), 'processPart3', $arg5);
+    return TRUE;
+  }
+  public function processPart1($arg1, $arg2) { sleep(10); return TRUE; }
+  public function processPart2($arg3, $arg4) { sleep(10); return TRUE; }
+  public function processPart3($arg5) { sleep(10); return TRUE; }
+  // */
+
+
+  /**
+   * Example: Run an upgrade with a query that touches many (potentially
+   * millions) of records by breaking it up into smaller chunks.
+   *
+   * @return TRUE on success
+   * @throws Exception
+  public function upgrade_4203() {
+    $this->ctx->log->info('Planning update 4203'); // PEAR Log interface
+
+    $minId = CRM_Core_DAO::singleValueQuery('SELECT coalesce(min(id),0) FROM civicrm_contribution');
+    $maxId = CRM_Core_DAO::singleValueQuery('SELECT coalesce(max(id),0) FROM civicrm_contribution');
+    for ($startId = $minId; $startId <= $maxId; $startId += self::BATCH_SIZE) {
+      $endId = $startId + self::BATCH_SIZE - 1;
+      $title = E::ts('Upgrade Batch (%1 => %2)', array(
+        1 => $startId,
+        2 => $endId,
+      ));
+      $sql = '
+        UPDATE civicrm_contribution SET foobar = whiz(wonky()+wanker)
+        WHERE id BETWEEN %1 and %2
+      ';
+      $params = array(
+        1 => array($startId, 'Integer'),
+        2 => array($endId, 'Integer'),
+      );
+      $this->addTask($title, 'executeSql', $sql, $params);
+    }
+    return TRUE;
+  } // */
+
+}
---
+++ genderpronouns.php
@@ -0,0 +1,184 @@
+<?php
+
+require_once 'genderpronouns.civix.php';
+use CRM_Genderpronouns_ExtensionUtil as E;
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/ 
+ */
+function genderpronouns_civicrm_config(&$config) {
+  _genderpronouns_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
+ */
+function genderpronouns_civicrm_xmlMenu(&$files) {
+  _genderpronouns_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function genderpronouns_civicrm_install() {
+  _genderpronouns_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function genderpronouns_civicrm_postInstall() {
+  _genderpronouns_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function genderpronouns_civicrm_uninstall() {
+  _genderpronouns_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function genderpronouns_civicrm_enable() {
+  _genderpronouns_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ */
+function genderpronouns_civicrm_disable() {
+  _genderpronouns_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function genderpronouns_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _genderpronouns_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
+ */
+function genderpronouns_civicrm_managed(&$entities) {
+  _genderpronouns_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
+ */
+function genderpronouns_civicrm_caseTypes(&$caseTypes) {
+  _genderpronouns_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
+ */
+function genderpronouns_civicrm_angularModules(&$angularModules) {
+  _genderpronouns_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
+ */
+function genderpronouns_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _genderpronouns_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function genderpronouns_civicrm_entityTypes(&$entityTypes) {
+  _genderpronouns_civix_civicrm_entityTypes($entityTypes);
+}
+
+/**
+ * Implements hook_civicrm_thems().
+ */
+function genderpronouns_civicrm_themes(&$themes) {
+  _genderpronouns_civix_civicrm_themes($themes);
+}
+
+function genderpronouns_civicrm_builForm($formName, $form) {
+  $genderOptions = civicrm_api3('OptionValue', 'get', ['name' => 'gender_pronouns', ['return' => ['label', 'value']]]);
+  if ($form->elementExists('custom_field_id')) {
+    $form->addElement('Select', 'pronoun_options', ts('Gender Pronoun'),
+      [
+        0 => ts('- select -'),
+      ] + $genderOptions,
+      FALSE,
+      ['class' => "crm-select2"]
+    );
+  }
+}
+ 
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ *
+function genderpronouns_civicrm_preProcess($formName, &$form) {
+
+} // */
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
+ *
+function genderpronouns_civicrm_navigationMenu(&$menu) {
+  _genderpronouns_civix_insert_navigation_menu($menu, 'Mailings', array(
+    'label' => E::ts('New subliminal message'),
+    'name' => 'mailing_subliminal_message',
+    'url' => 'civicrm/mailing/subliminal',
+    'permission' => 'access CiviMail',
+    'operator' => 'OR',
+    'separator' => 0,
+  ));
+  _genderpronouns_civix_navigationMenu($menu);
+} // */
